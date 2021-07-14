c5581529cec49a5e42d1af432bf34e5d7c175ed3
chaks03 <chakshu@samagragovernance.in>
2021-07-14 23:37:09 +0530

    Refactor: Rename Kafka Producer

---
+++ src/main/java/com/uci/utils/kafka/SimpleProducer.java
@@ -0,0 +1,38 @@
+package com.uci.utils.kafka;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Service;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+
+@Service
+@Slf4j
+public class SimpleProducer {
+
+    private final KafkaTemplate<String, String> simpleProducer;
+
+    public SimpleProducer(KafkaTemplate<String, String> simpleProducer1) {
+        this.simpleProducer = simpleProducer1;
+    }
+
+    public void send(String topic, String message) {
+        simpleProducer
+                .send(topic, message)
+                .addCallback(new ListenableFutureCallback<SendResult<String, String>>() {
+                    @Override
+                    public void onFailure(@NotNull Throwable throwable) {
+                        log.error("Unable to push {} to {} topic due to {}", message, topic, throwable.getMessage());
+                    }
+
+                    @Override
+                    public void onSuccess(SendResult<String, String> stringStringSendResult) {
+                        log.info("Pushed to topic {}", topic);
+                    }
+                });
+    }
+}
