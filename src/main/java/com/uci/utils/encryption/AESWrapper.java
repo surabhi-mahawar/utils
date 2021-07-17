4d9c738d07ba09266277ec9bc79737aaa8f594dc
chaks03 <chakshu@samagragovernance.in>
2021-07-17 21:26:21 +0530

    Feat: Added AES Wrapper Utility

---
+++ src/main/java/com/uci/utils/encryption/AESWrapper.java
@@ -0,0 +1,46 @@
+package com.uci.utils.encryption;
+
+import java.security.Key;
+import java.util.Base64;
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+
+public class AESWrapper {
+
+    private static final String ALGO = "AES"; // Default uses ECB PKCS5Padding
+
+    public static String encrypt(String Data, String secret) throws Exception {
+        Key key = generateKey(secret);
+        Cipher c = Cipher.getInstance(ALGO);
+        c.init(Cipher.ENCRYPT_MODE, key);
+        byte[] encVal = c.doFinal(Data.getBytes());
+        return Base64.getEncoder().encodeToString(encVal);
+    }
+
+    public static String decrypt(String strToDecrypt, String secret) {
+        try {
+            Key key = generateKey(secret);
+            Cipher cipher = Cipher.getInstance(ALGO);
+            cipher.init(Cipher.DECRYPT_MODE, key);
+            return new String(cipher.doFinal(Base64.getDecoder().decode(strToDecrypt)));
+        } catch (Exception e) {
+            System.out.println("Error while decrypting: " + e.toString());
+        }
+        return null;
+    }
+
+    private static Key generateKey(String secret) throws Exception {
+        byte[] decoded = Base64.getDecoder().decode(secret.getBytes());
+        return new SecretKeySpec(decoded, ALGO);
+    }
+
+    public static String decodeKey(String str) {
+        byte[] decoded = Base64.getDecoder().decode(str.getBytes());
+        return new String(decoded);
+    }
+
+    public static String encodeKey(String str) {
+        byte[] encoded = Base64.getEncoder().encode(str.getBytes());
+        return new String(encoded);
+    }
+}
