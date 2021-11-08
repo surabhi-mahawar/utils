951cb3d802c488d09c539337f990f4ff0573f94e
Surabhi <surabhi.mahawar@aurigait.com>
2021-11-08 11:42:16 +0530

    minor changes

--- src/main/java/com/uci/utils/UtilAppConfiguration.java
+++ src/main/java/com/uci/utils/UtilAppConfiguration.java
@@ -1,10 +1,15 @@
 package com.uci.utils;
 
+import java.time.Duration;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import reactor.netty.http.client.HttpClient;
 
 
 @Configuration
@@ -20,7 +25,10 @@ public class UtilAppConfiguration {
 
     @Bean
     public WebClient getWebClient() {
+//    	HttpClient httpClient = HttpClient.create()
+//    			  .responseTimeout(Duration.ofSeconds(1)); 
         return WebClient.builder()
+//        		.clientConnector(new ReactorClientHttpConnector(httpClient))
                 .baseUrl(CAMPAIGN_URL)
                 .defaultHeader("admin-token", CAMPAIGN_ADMIN_TOKEN)
                 .build();
