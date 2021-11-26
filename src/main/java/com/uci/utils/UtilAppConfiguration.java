0c73374b6986b82a8e0cf1f136b069fe2f0d3735
Surabhi <surabhi.mahawar@aurigait.com>
2021-11-26 14:32:41 +0530

    caffeine cache changes

--- src/main/java/com/uci/utils/UtilAppConfiguration.java
+++ src/main/java/com/uci/utils/UtilAppConfiguration.java
@@ -1,30 +1,49 @@
 package com.uci.utils;
 
+import java.time.Duration;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
 
 @Configuration
 @EnableAutoConfiguration
 public class UtilAppConfiguration {
 
+	@Value("${campaign.url}")
+	public String CAMPAIGN_URL;
 
-    @Value("${campaign.url}")
-    public String CAMPAIGN_URL;
-    
 	@Value("${campaign.admin.token}")
 	public String CAMPAIGN_ADMIN_TOKEN;
 
-    @Bean
-    public WebClient getWebClient() {
-        return WebClient.builder()
-                .baseUrl(CAMPAIGN_URL)
-                .defaultHeader("admin-token", CAMPAIGN_ADMIN_TOKEN)
-                .build();
-    }
+	@Value("${caffeine.cache.max.size}")
+	public Integer cacheMaxSize;
+	
+	@Value("${caffeine.cache.exprie.duration.seconds}")
+	public Integer cacheExpireDuration;
+	
+	public Caffeine<Object, Object> caffeineCacheBuilder() {
+		return Caffeine.newBuilder()
+				.maximumSize(cacheMaxSize)
+				.expireAfterWrite(Duration.ofSeconds(cacheExpireDuration))
+				.recordStats();
+	}
 
+	@Bean
+	public Cache<Object, Object> cache() {
+		return caffeineCacheBuilder().build();
+	}
+
+	@Bean
+	public WebClient getWebClient() {
+		return WebClient.builder().baseUrl(CAMPAIGN_URL).defaultHeader("admin-token", CAMPAIGN_ADMIN_TOKEN).build();
+	}
 
 }
